734e522b7fba85dfb2c1916f9be836be9e2dfc72
Sofia azahra <sofiaazahra20@gmail.com>
2024-02-03 17:08:41 +0700

    Tugas Template Soffia

---
+++ resources/views/try.blade.php
@@ -0,0 +1,8 @@
+@extends('template2.master')
+
+@section('header', 'try')
+
+@section('content')
+<button class='"btn btn-primary'>HELLO</button>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/template2', function () {
+    return view('template2.master');
+});
+
+Route::get('/try', function () {
+    return view('try');
 });
